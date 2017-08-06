2a1e9702c9869d0da9302e12f3eafb546ff05e60
Dinesh KD <dinesh.net@live.com>
2017-08-06 10:11:08 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PasswordResetComponent } from './auth/password-reset/password-reset.component';
+import { ForgetPasswordComponent } from './auth/forget-password/forget-password.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    LoginComponent,
+    PasswordResetComponent,
+    ForgetPasswordComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
